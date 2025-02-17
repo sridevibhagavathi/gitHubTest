ba4dd077ef70bc94c680e9e198106d5193b315c8
moonsridevi@gmail.com <moonsridevi@gmail.comg>
2025-02-17 20:33:57 +0530

    Webdriverfile

---
+++ src/test/java/sample1/SampleTest1.java
@@ -0,0 +1,54 @@
+package sample1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class SampleTest1 {
+	
+	//write in test ng will get one error in jenkin for testng want to see that , 
+	//so create class files in test ng
+	//rightclick project and add testng libraries
+	
+	WebDriver wd;
+	@Test
+	public void loadGoogle(){
+		
+		//webdrivermanager().chromedriver().setup();
+		
+		//load browser with old version code(sytem. setproperty)
+		//check chrome verion in your system chrome browser
+		//go to google search with google download, match with yur chrome version , there is exe file of all versions
+		//that version will be pased in system.set properly value, it was used in old version automation testing
+		//webdrivermanager().chromedriver().setup(); - this is latest one
+		//webdriver.chrome.driverf - thisis default key, which they provided, use only it
+		//System.setProperty("webdriver.chrome.driverf", "download exe file in your local, paste that path here");
+			ChromeOptions opt=new ChromeOptions();
+			opt.addArguments("--remote-allow-origins=*");
+			WebDriver wd=new ChromeDriver();
+			wd.manage().window().maximize();
+			wd.get("https://www.google.com/");
+			wd.findElement(By.name("q")).sendKeys("Chennai");
+			wd.findElement(By.name("q")).submit();
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() throws IOException{
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:/Users/Sridevi Bhagavathi/workspace/GitHubTest/screenshot/google.png"));
+	}
+	
+
+}
